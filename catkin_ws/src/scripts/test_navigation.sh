064328a4537d18b64769f0356a9fc430b7438aa0
Vaibhav Patel <vai.pateln@gmail.com>
2020-06-07 20:25:43 -0700

    Set robot initial_pose_a
    AMCL helpfully has some launch params to set the initial pose angle on startup so I do not have to do 2D Pose Estimate

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -7,6 +7,6 @@ export TURTLEBOT_GAZEBO_MAP_FILE=$(rospack find my_robot)/maps/SumWorldMap.yaml
 # Now we launch various launch files.
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch initial_pose_a:=-1.5707" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
